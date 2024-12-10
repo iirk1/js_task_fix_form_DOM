da4c5843406fd4c4403f6795d7ce9d6f5c6d5d62
Iryna <irakogut23190@gmail.com>
2024-12-10 16:27:11 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+let inputsArray = Array.from(inputs);
+
+console.log(inputsArray);
+
+for (const input of inputsArray) {
+  input.insertAdjacentHTML(
+    'beforebegin',
+    `<label class = 'field-label' for = "${input.id}" > ${input.name} </label>`,
+  );
+  const placeholderText = `${input.name.toLocaleLowerCase()}`;
+
+  const capitalizeLetter =
+    placeholderText.charAt(0).toLocaleUpperCase() + placeholderText.slice(1);
+
+  input.placeholder = capitalizeLetter;
+}
