1fc3cad1bfeb25ebddd4cae45749f57f7c3e5433
Iryna <irakogut23190@gmail.com>
2024-12-11 14:48:57 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,6 @@ const inputs = document.querySelectorAll('input');
 
 let inputsArray = Array.from(inputs);
 
-console.log(inputsArray);
-
 for (const input of inputsArray) {
   input.insertAdjacentHTML(
     'beforebegin',
@@ -17,4 +15,12 @@ for (const input of inputsArray) {
     placeholderText.charAt(0).toLocaleUpperCase() + placeholderText.slice(1);
 
   input.placeholder = capitalizeLetter;
+
+  if (input.placeholder === 'Firstname') {
+    input.placeholder = 'First name';
+  }
+
+  if (input.placeholder === 'Lastname') {
+    input.placeholder = 'Last name';
+  }
 }
